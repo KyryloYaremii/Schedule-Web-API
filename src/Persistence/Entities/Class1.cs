eaab825401b8b429cee14920cbf37a2ff0edea2c
@Obsessed_KiRi <kirillyaremiy12@gmail.com>
2025-09-03 23:23:02 +0300

    Create persistence layer

--- src/Persistence/Entities/Class1.cs
+++ src/Persistence/Entities/Class1.cs
@@ -1,4 +1,4 @@
-﻿namespace Persistence
+﻿namespace Persistence.Entities
 {
     public class Class1
     {
--- src/Schedule-Web-API/Controllers/UniversityController.cs
+++ src/Schedule-Web-API/Controllers/UniversityController.cs
@@ -1,6 +1,33 @@
-﻿namespace Schedule_Web_API.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Schedule_Web_API.Controllers;
+
+[Route("university")]
+[ApiController]
+public class UniversityController : ControllerBase
 {
-    public class UniversityController
+    [HttpGet("{university}")]
+    public async Task<IActionResult> GetSchedule(string university)
     {
+        if (!string.IsNullOrWhiteSpace(university))
+        {
+            return BadRequest("Некорректные параметры запроса.");
+        }
+
+        switch (university.ToLower())
+        {
+            case "lpnu":
+                schedule = await _parser.GetScheduleAsync(group, semester);
+                break;
+            default:
+                return NotFound("Расписание для данного университета не поддерживается.");
+        }
+
+        if (schedule.Count == 0)
+        {
+            return NotFound("Расписание не найдено.");
+        }
+
+        return Ok(schedule);
     }
 }
